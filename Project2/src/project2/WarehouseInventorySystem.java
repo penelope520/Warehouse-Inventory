fbe24bdcab557faca2ccc683cc545e1ec4a6b5c0
719 <rqy1234@aut.uni.ac.nz>
2024-08-27 18:37:04 +1200

    MainGui setUp

--- Project2/src/project2/WarehouseInventorySystem.java
+++ Project2/src/project2/WarehouseInventorySystem.java
@@ -4,17 +4,53 @@
  */
 package project2;
 
-/**
- *
- * @author Administrator
- */
-public class Project2 {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class WarehouseInventorySystem{
+
+    JFrame Maingui=new JFrame("Userlogin System");
+    JLabel title=new JLabel("Online Shopping/Management System");
+    JLabel username=new JLabel("Username: ");
+    JLabel password=new JLabel("Password");
+    JLabel usertype=new JLabel("Identity");
+    JTextField name=new JTextField();
+    JTextField pwd=new JPasswordField();
+    JComboBox box=new JComboBox(new String[]{"Administrator","Customer"});
+    JButton login=new JButton("Login");
+    JButton signUp=new JButton("SignUp");
+    
+    public void Maingui(){
+        Maingui.setBounds(450, 200, 550, 350);
+        Maingui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Maingui.setLayout(new GridLayout(6,2,10,10));
+        
+        Maingui.add(title);
+        Maingui.add(login);
+        Maingui.add(signUp);
+        
+        title.setFont(new Font("Arial", Font.BOLD, 16));
+        Maingui.add(new JLabel(""));
+        
+        
+        Maingui.setLocationRelativeTo(null);
+        Maingui.setVisible(true);
     }
     
+    public static void main(String[] args) {
+        WarehouseInventorySystem wis=new WarehouseInventorySystem();
+        wis.Maingui();
+        
+    }
+    class Background extends JPanel{
+        private Image backgroundImage;
+        
+        protected void paintComonent(Graphics g){
+            backgroundImage = new ImageIcon("Network-Inventory-Management.png").getImage();
+            g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+        }
+    
+    }
 }
